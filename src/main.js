385541fb84a74121ee749c8480a588a8cea45771
gaokai <gaokai@qima-inc.comgit>
2016-05-19 17:01:27 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,37 +1,10 @@
-import './raf_polyfill'
+import React from 'react'
+import ReactDom from 'react-dom'
+import DragonBoatPage from '@youzan/dragon-boat-fullpage'
+import 'normalize.css'
 import './index.scss'
 
-import Brush from './Brush'
+const preventScroll = e => e.preventDefault()
+window.addEventListener('touchmove', preventScroll)
 
-window.addEventListener('touchmove', e => e.preventDefault())
-
-const canvas = document.querySelector('canvas')
-canvas.width = 500;
-canvas.height = 500;
-
-const img = new Image()
-img.src = 'http://raistlin916.github.io/brush/assets/ink.png'
-img.onload = () => {
-  const brush = new Brush(canvas, img)
-
-  function start() {
-    let lastTime = 0
-    let appTime = 0
-    function r(time) {
-      requestAnimationFrame(r)
-      let dt = time - lastTime
-      if (dt < 10) return
-      if (dt > 100) dt = 16
-      lastTime = time
-      dt /= 1000
-      appTime += dt
-
-      brush.update(dt)
-      brush.render()
-    }
-    requestAnimationFrame(r)
-  }
-
-  start()
-}
-
+ReactDom.render(<DragonBoatPage />, document.getElementById('container'))
